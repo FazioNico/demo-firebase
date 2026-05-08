bd20553c23807f2bf44461c59b12e0a15920084b
FazioNico <contact@nicolasfazio.ch>
2026-05-08 10:07:49 +0200

    feat: enhance todo management with completion status and update functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,8 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FireService } from './services/fire/fire';
+import { FireService, TodoInterface } from './services/fire/fire';
 import { AsyncPipe } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,15 @@ import { AsyncPipe } from '@angular/common';
 export class App {
   protected readonly title = signal('demo-fire');
   private readonly _fireService = inject(FireService);
-  public readonly todosList$ = this._fireService.loadTodos();
+  public readonly todosList$: Observable<TodoInterface[]> = this._fireService.loadTodos();
 
   async handleAdd() {
     this._fireService.addDoc({
       title: 'Il est ' + new Date().toLocaleTimeString()
     });
+  }
+
+  async handleDone(todoId: string) {
+    await this._fireService.done(todoId);
   }
 }
--- src/app/services/fire/fire.ts
+++ src/app/services/fire/fire.ts
@@ -1,8 +1,13 @@
 import { inject, Injectable } from '@angular/core';
-import { collection, collectionData, doc, Firestore, query, setDoc } from '@angular/fire/firestore';
+import { addDoc, collection, collectionData, doc, Firestore, query, setDoc, updateDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { v4 as uuidv4 } from "uuid";
 
+export interface TodoInterface {
+  title: string; 
+  id: string;
+  done?: boolean;
+}
 @Injectable({
   providedIn: 'root',
 })
@@ -15,12 +20,24 @@ export class FireService {
     await setDoc(docRef, {...data}).catch(error => {
       console.log('error firebase', )
     });
+
+    // const colRef = collection(this._fire, 'nomades-todos-list');
+    // await addDoc(colRef, data).catch(error => {
+    //   console.log('error: ', error);
+    // });
   }
 
   loadTodos() {
     const colRef = collection(this._fire, 'nomades-todos-list');
     const q = query(colRef);
-    const datas$ = collectionData(q, {idField: 'id'}) as Observable<{title: string; id: string;}[]>;
+    const datas$ = collectionData(q, {idField: 'id'}) as Observable<TodoInterface[]>;
     return datas$;
+  }
+
+  async done(id: string) {
+    const docRef = doc(this._fire, `nomades-todos-list/${id}`);
+    await updateDoc(docRef, {done: true, updatedAt: new Date().toISOString()}).catch(erreur => {
+      console.log('error: ', erreur);
+    });
   }
 }
